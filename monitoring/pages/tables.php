ebc62208febc779309e478849e7bb4d2d3e7eb95
muhammad aji <95458296+achidoang@users.noreply.github.com>
2024-05-19 10:15:32 +0700

    ganti judul tabel

--- monitoring/pages/tables.php
+++ monitoring/pages/tables.php
@@ -92,7 +92,7 @@
     <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" data-scroll="false">
       <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
-          <h6 class="font-weight-bolder text-white mb-0">User Data</h6>
+          <h6 class="font-weight-bolder text-white mb-0">History Monitoring</h6>
         </nav>
         <!-- Tempatkan tombol dark mode di sini -->
         <div class="form-check form-switch ps-0 ms-auto my-auto">
