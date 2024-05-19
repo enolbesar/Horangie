715ce46e39b789b353fe3a5ed3868c5b486bc5aa
muhammad aji <95458296+achidoang@users.noreply.github.com>
2024-05-19 13:40:43 +0700

    fix bug link active pada navside

--- monitoring/pages/Template/nav.php
+++ monitoring/pages/Template/nav.php
@@ -1,4 +1,7 @@
-
+<?php
+// Get the current file name
+$current_page = basename($_SERVER['PHP_SELF']);
+?>
 <div class="min-height-300 bg-primary position-absolute w-100"></div>
 <aside
     class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 "
@@ -6,17 +9,17 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html "
+        <a class="navbar-brand m-0" href="https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html"
             target="_blank">
             <img src="../assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1 font-weight-bold">Horangie Dashboard</span>
         </a>
     </div>
     <hr class="horizontal dark mt-0">
-    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
-        <ul class="navbar-nav">
+    <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
+        <ul class="navbar-nav" id="nav-list">
             <li class="nav-item">
-                <a class="nav-link active" href="../pages/dashboard.php">
+                <a class="nav-link <?php echo $current_page == 'dashboard.php' ? 'active' : ''; ?>" href="../pages/dashboard.php">
                     <div
                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
@@ -25,16 +28,16 @@
                 </a>
             </li>
             <li class="nav-item">
-        <a class="nav-link" href="../pages/tables.php">
-        <div
-            class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="ni ni-chart-bar-32 text-primary text-sm opacity-10"></i>
-        </div>
-        <span class="nav-link-text ms-1">Table</span>
-        </a>
-    </li>
+                <a class="nav-link <?php echo $current_page == 'tables.php' ? 'active' : ''; ?>" href="../pages/tables.php">
+                    <div
+                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-chart-bar-32 text-primary text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Table</span>
+                </a>
+            </li>
             <li class="nav-item">
-                <a class="nav-link " href="../pages/userData.php">
+                <a class="nav-link <?php echo $current_page == 'userData.php' ? 'active' : ''; ?>" href="../pages/userData.php">
                     <div
                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="ni ni-bullet-list-67 text-primary text-sm opacity-10"></i>
@@ -43,13 +46,13 @@
                 </a>
             </li>
             <li class="nav-item">
-        <a class="nav-link" href="../pages/registration.php">
-        <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="ni ni-check-bold text-primary text-sm opacity-10"></i>
-        </div>
-        <span class="nav-link-text ms-1">Registration</span>
-        </a>
-    </li>
+                <a class="nav-link <?php echo $current_page == 'registration.php' ? 'active' : ''; ?>" href="../pages/registration.php">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-check-bold text-primary text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Registration</span>
+                </a>
+            </li>
+        </ul>
     </div>
 </aside>
-
