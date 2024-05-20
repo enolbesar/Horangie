bedb9682fddd8354875b96d306fbb2663520f514
muhammad aji <95458296+achidoang@users.noreply.github.com>
2024-05-20 11:51:25 +0700

    memperbaiki UI data realtime

--- monitoring/pages/Template/dataRealTime.php
+++ monitoring/pages/Template/dataRealTime.php
@@ -1,15 +1,15 @@
 <div class="row">
     <div class="col-lg-2 col-sm-6 mb-xl-0 mb-4">
         <div class="card">
-            <div class="card-body d-flex flex-column align-items-left p-3">
+            <div class="card-body d-flex flex-column align-items-left p-1">
                 <div class="icon text-primary mb-0">
                     <i class="fas fa-mountain text-lg opacity-10" aria-hidden="true"></i>
                 </div>
                 <div class="text-center flex-grow-1">
-                    <h2 class="font-weight-bolder mt-2">
+                    <h2 class="font-weight-bolder  mt-1">
                         <span id="ceksoil"></span>
                     </h2>
-                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Soil Moisture</p>
+                    <p class="text-sm mb-2 text-uppercase font-weight-bold">Soil Moisture</p>
                 </div>
             </div>
         </div>
@@ -17,15 +17,15 @@
 
     <div class="col-xl-2 col-sm-6 mb-xl-0 mb-4">
         <div class="card">
-            <div class="card-body d-flex flex-column align-items-left p-3"> <!-- Gaya disesuaikan -->
+            <div class="card-body d-flex flex-column align-items-left p-1"> <!-- Gaya disesuaikan -->
                 <div class="icon text-primary mb-0">
                     <i class="fas fa-thermometer-half text-lg opacity-10" aria-hidden="true"></i>
                 </div>
                 <div class="text-center flex-grow-1">
-                    <h2 class="font-weight-bolder mt-2">
+                    <h2 class="font-weight-bolder mt-1">
                         <span id="cektemp"></span> °C
                     </h2>
-                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Air Temperature</p>
+                    <p class="text-sm mb-2 text-uppercase font-weight-bold">Air Temperature</p>
                 </div>
             </div>
         </div>
@@ -33,15 +33,15 @@
 
     <div class="col-xl-2 col-sm-6 mb-xl-0 mb-4">
         <div class="card">
-            <div class="card-body d-flex flex-column align-items-left p-3"> <!-- Gaya disesuaikan -->
+            <div class="card-body d-flex flex-column align-items-left p-1"> <!-- Gaya disesuaikan -->
                 <div class="icon text-primary mb-0">
                     <i class="fas fa-tint text-lg opacity-10" aria-hidden="true"></i>                        
                 </div>
                 <div class="text-center flex-grow-1">
-                    <h2 class="font-weight-bolder mt-2">
+                    <h2 class="font-weight-bolder mt-1">
                         <span id="cekhum"></span>
                     </h2>
-                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Air Humidity</p>
+                    <p class="text-sm mb-2 text-uppercase font-weight-bold">Air Humidity</p>
                 </div>
             </div>
         </div>
@@ -49,15 +49,15 @@
 
     <div class="col-xl-2 col-sm-6 mb-xl-0 mb-4">
         <div class="card">
-            <div class="card-body d-flex flex-column align-items-left p-3"> <!-- Gaya disesuaikan -->
+            <div class="card-body d-flex flex-column align-items-left p-1"> <!-- Gaya disesuaikan -->
                 <div class="icon text-primary mb-0">
                     <i class="fas fa-thermometer text-lg opacity-10" aria-hidden="true"></i>
                 </div>
                 <div class="text-center flex-grow-1">
-                    <h2 class="font-weight-bolder mt-2">
+                    <h2 class="font-weight-bolder mt-1">
                         <span id="cekhindex"></span>
                     </h2>
-                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Heat Index</p>
+                    <p class="text-sm mb-2 text-uppercase font-weight-bold">Heat Index</p>
                 </div>
             </div>
         </div>
@@ -65,15 +65,15 @@
 
     <div class="col-xl-2 col-sm-6 mb-xl-0 mb-4">
         <div class="card">
-            <div class="card-body d-flex flex-column align-items-left p-3"> <!-- Gaya disesuaikan -->
+            <div class="card-body d-flex flex-column align-items-left p-1"> <!-- Gaya disesuaikan -->
                 <div class="icon text-primary mb-0">
                     <i class="fas fa-sun text-lg opacity-10" aria-hidden="true"></i>
                 </div>
                 <div class="text-center flex-grow-1">
-                    <h2 class="font-weight-bolder mt-2">
+                    <h2 class="font-weight-bolder mt-1">
                         <span id="ceksun"></span>
                     </h2>
-                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Sunlight</p>
+                    <p class="text-sm mb-2 text-uppercase font-weight-bold">Sunlight</p>
                 </div>
             </div>
         </div>
@@ -81,15 +81,15 @@
 
     <div class="col-xl-2 col-sm-6 mb-xl-0 mb-4">
         <div class="card">
-            <div class="card-body d-flex flex-column align-items-left p-3"> <!-- Gaya disesuaikan -->
+            <div class="card-body d-flex flex-column align-items-left p-1"> <!-- Gaya disesuaikan -->
                 <div class="icon text-primary mb-0">
                     <i class="fas fa-cloud-showers-heavy text-lg opacity-10" aria-hidden="true"></i>
                 </div>
                 <div class="text-center flex-grow-1">
-                    <h2 class="font-weight-bolder mt-2">
+                    <h2 class="font-weight-bolder mt-1">
                         <span id="cekrain"></span>
                     </h2>
-                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Rain Intensity</p>
+                    <p class="text-sm mb-2 text-uppercase font-weight-bold">Rain Intensity</p>
                 </div>
             </div>
         </div>
